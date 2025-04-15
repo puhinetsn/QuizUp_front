1d487cf7e88bf59c3dfeacfc2ee43e7860e37d04
puhinetsn <puhinetsn82@gmail.com>
2025-04-15 22:46:00 +0300

    environment variables added

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -2,15 +2,16 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Category } from '../models/categories.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoriesService {
   private http = inject(HttpClient);
-  private expressUrl = 'http://localhost:3000/api';
+  private apiUrl = environment.apiUrl;
 
   public getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(`${this.expressUrl}/categories`);
+    return this.http.get<Category[]>(`${this.apiUrl}/categories`);
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://quizup-back.onrender.com/api',
+};
